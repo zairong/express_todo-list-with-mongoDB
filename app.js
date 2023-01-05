c6378238a92484ac10f1242905608e312e776794
zairong <justiceman3211@hotmail.com>
2023-01-05 14:21:43 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
@@ -11,3 +32,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
