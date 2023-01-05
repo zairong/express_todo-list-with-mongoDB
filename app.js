f843b6f11df4c2b282b17a3c52d3cbc166471abe
zairong <justiceman3211@hotmail.com>
2023-01-05 15:28:12 +0800

    modify index.hbs

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 設定首頁路由
+const Todo = require('./models/todo') // 載入 Todo model
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
