e2e91478761371866e01606d5ac0e4d84c082abc
zairong <justiceman3211@hotmail.com>
2023-01-05 17:54:09 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const app = express()
-// 引用路由器
 const routes = require('./routes')
-// 引用 body-parser
 const bodyParser = require('body-parser')
-const Todo = require('./models/todo') // 載入 Todo model
-// 載入 method-override
 const methodOverride = require('method-override') 
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 //設定樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
