218d7d8b1fa6752057924fafc6aecf43a99fb3df
Zairong <justiceman3211@hotmail.com>
2023-01-08 20:23:06 +0800

    add user login and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+//登入頁面路由
 router.get('/login', (req, res) => {
   res.render('login')
 })
-
+//傳送登入資料路由
+router.post('/login', (req, res) => {
+})
+//註冊頁面路由
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 module.exports = router
