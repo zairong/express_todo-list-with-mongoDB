eb12f55a89d8e523f11001bde2951abbc2a7da84
Zairong <justiceman3211@hotmail.com>
2023-01-11 18:30:29 +0800

    add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const User = require('../../models/user')
 // 引用 passport
 const passport = require('passport')
+const bcrypt = require('bcryptjs')  // 載入套件
 
 //登入頁面路由
 router.get('/login', (req, res) => {
@@ -21,37 +22,51 @@ router.get('/register', (req, res) => {
 })
 //傳送註冊資料的路由
 router.post('/register', (req, res) => {
-  // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
-  // 檢查使用者是否已經註冊
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
-    // 如果已經註冊：退回原本畫面
     if (user) {
-      console.log('User already exists.')
-      // alert('此帳號或信箱已註冊')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      // alert('註冊成功')
-       User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
     }
+    return bcrypt
+    .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+    .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+    .then(hash => User.create({
+      name,
+      email,
+      password: hash // 用雜湊值取代原本的使用者密碼
+    }))
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
   })
-  .catch(err => console.log(err))
+
 })
 //登出路由
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
